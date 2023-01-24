11b5db07728927044c99868250d4c6f251cfc8ae
desta2000 desta2000 <shlomodesta1@gmail.com>
2023-01-24 20:12:44 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 4040;
+
+app.get('/',(req,res)=>{
+    return res.status(200).json({msg:"Hello Ninja"});
+});
+
+app.listen(port,()=>{
+    console.log("server started")
+})
